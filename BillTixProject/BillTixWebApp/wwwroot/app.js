a3afb261d66d019d3c38134ac31a027a82ad43cb
Sajor <your-email@example.com>
2026-06-29 20:23:35 +0800

    123

---
+++ BillTixProject/BillTixWebApp/wwwroot/app.js
@@ -0,0 +1,8 @@
+window.downloadFile = function (base64, fileName, mimeType) {
+    const link = document.createElement('a');
+    link.href = `data:${mimeType};base64,${base64}`;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
